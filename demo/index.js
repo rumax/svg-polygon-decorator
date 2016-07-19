c6a613cd328b64523f814cac64cb1c4cc163c90b
Maksym Rusynyk <m.rusynyk@gmail.com>
2016-07-19 11:58:35 +0200

    more examples. minor refactoring

--- demo/index.js
+++ demo/index.js
@@ -1,29 +1,39 @@
 import svgCloud from '../src/decorator';
 
 const form = document.querySelector('.controls');
+const clouds = [
+  {
+    selector: '#cloud1',
+    coords: [[10, 120], [180, 120], [230, 180]]
+  },
+  {
+    selector: '#cloud2',
+    coords: [[20, 300], [180, 300],  [200, 330], [190, 360], [140, 420],
+             [70, 380]]
+  },
+  {
+    selector: '#cloud3',
+    coords: [[460, 120], [290, 120], [240, 180]]
+  },
+  {
+    selector: '#cloud4',
+    coords: [[450, 300], [290, 300], [270, 330], [280, 360], [330, 420],
+             [400, 380]]
+  }
+];
+
 
 const render = () => {
-  const cloudEl1 = document.querySelector('#cloud1');
-  const cloudEl2 = document.querySelector('#cloud2');
+  let elem, coords;
+  let cloud;
+  const radius = parseFloat(form['radius'].value);
 
-  let radius = parseFloat(form['radius'].value);
-
-  var coordinates = [
-    [10, 120], [180, 120], [230, 180]
-    //[230, 180], [180, 120], [10, 120]
-  ];
-
-  let cloud1 = svgCloud(coordinates, radius);
-  cloudEl1.setAttribute('d', cloud1);
-
-  let coordinates2 = [
-    [20, 300], [180, 300],  [200, 330], [190, 360], [140, 420], [70, 380]
-  ];
-
-  let cloud2 = svgCloud(coordinates2, radius);
-  cloudEl2.setAttribute('d', cloud2);
+  for (let ind = 0, cnt = clouds.length; ind < cnt; ++ind) {
+    elem = document.querySelector(clouds[ind].selector);
+    cloud = svgCloud(clouds[ind].coords, radius);
+    elem.setAttribute('d', cloud);
+  }
 }
 
-render();
-
 form.addEventListener('change', render);
+render();
--- src/decorator.js
+++ src/decorator.js
@@ -7,59 +7,60 @@
  * @return {Path}
  */
 
-let lineSegmentIntersection = (circle, radius, pointA, pointB) => {
-  const xShift = circle.x;
-  const yShift = circle.y;
+// let lineSegmentIntersection = (circle, radius, pointA, pointB) => {
+//   const xShift = circle.x;
+//   const yShift = circle.y;
+//
+//   //Recet circle to (0,0) coordinate
+//   circle.x = 0;
+//   circle.y = 0;
+//   pointA.x -= xShift;
+//   pointA.y -= yShift;
+//   pointB.x -= xShift;
+//   pointB.y -= yShift;
+//
+//   let k = (pointB.y - pointA.y) / (pointB.x - pointA.x);
+//   let b = pointA.y - pointA.x * (pointB.y - pointA.y) / (pointB.x - pointA.x);
+//
+//   const A = 1 + k * k;
+//   const B = 2 * k * b;
+//   const C = b * b - radius * radius;
+//   const D = B * B - 4 * A * C;
+//
+//   let x1 = null;
+//   let x2 = null;
+//   let y1 = null;
+//   let y2 = null;
+//
+//   if (0 < D) {
+//     console.log('2 points intersects');
+//     x1 = (-B + Math.sqrt(D)) / (2 * A);
+//     x2 = (-B - Math.sqrt(D)) / (2 * A);
+//
+//     if (pointA.x < x1 && pointB.x > x1) {
+//       return [x1 + xShift, (x1 * k  + b) + yShift];
+//     }
+//   } else if (0 === D) {
+//     console.log('1 point intersect');
+//     x1 = x2 = (-B + Math.sqrt(D)) / (2 * A);
+//   } else {
+//     console.log('0 points intersect');
+//   }
+//
+//   return null !== x2 ? [x2 + xShift, (x2 * k  + b) + yShift] : null;
+// };
 
-  //Recet circle to (0,0) coordinate
-  circle.x = 0;
-  circle.y = 0;
-  pointA.x -= xShift;
-  pointA.y -= yShift;
-  pointB.x -= xShift;
-  pointB.y -= yShift;
-
-  let k = (pointB.y - pointA.y) / (pointB.x - pointA.x);
-  let b = pointA.y - pointA.x * (pointB.y - pointA.y) / (pointB.x - pointA.x);
-
-  const A = 1 + k * k;
-  const B = 2 * k * b;
-  const C = b * b - radius * radius;
-  const D = B * B - 4 * A * C;
-
-  let x1 = null;
-  let x2 = null;
-  let y1 = null;
-  let y2 = null;
-
-  if (0 < D) {
-    console.log('2 points intersects');
-    x1 = (-B + Math.sqrt(D)) / (2 * A);
-    x2 = (-B - Math.sqrt(D)) / (2 * A);
-
-    if (pointA.x < x1 && pointB.x > x1) {
-      return [x1 + xShift, (x1 * k  + b) + yShift];
-    }
-  } else if (0 === D) {
-    console.log('1 point intersect');
-    x1 = x2 = (-B + Math.sqrt(D)) / (2 * A);
-  } else {
-    console.log('0 points intersect');
-  }
-
-  return null !== x2 ? [x2 + xShift, (x2 * k  + b) + yShift] : null;
-};
+const X = 0;
+const Y = 1;
+const FLOAT_PRECISE = 0.000001;
 
 
 let lineLength = (line) => {
   let pointA = line[0];
   let pointB = line[1];
-  let xA = pointA[0];
-  let xB = pointB[0];
-  let yA = pointA[1];
-  let yB = pointB[1];
 
-  return Math.sqrt(Math.pow((xB - xA), 2) + Math.pow((yB - yA), 2));
+  return Math.sqrt(Math.pow((pointB[X] - pointA[X]), 2) +
+    Math.pow((pointB[Y] - pointA[Y]), 2));
 };
 
 
@@ -67,63 +68,38 @@ let getIntersection = (line, length) => {
   let pointC = null;
   let pointA = line[0];
   let pointB = line[1];
-  let xA = pointA[0];
-  let xB = pointB[0];
-  let yA = pointA[1];
-  let yB = pointB[1];
   let delta;
   let xPoint;
   let yPoint;
-
   let fullLen = lineLength(line);
-  const ESP = 0.001;
-  if (Math.abs(fullLen - length) < ESP) {
+
+  if (Math.abs(fullLen - length) < FLOAT_PRECISE) {
     pointC = pointB;
   } else if (fullLen > length) {
     delta = length / (fullLen - length)
-    xPoint = (xA + delta * xB) / (1 + delta);
-    yPoint = (yA + delta * yB) / (1 + delta);
+    xPoint = (pointA[X] + delta * pointB[X]) / (1 + delta);
+    yPoint = (pointA[Y] + delta * pointB[Y]) / (1 + delta);
     pointC = [xPoint, yPoint];
   }
 
   return pointC;
 };
 
+
 let cloudALine = (line, radius, nextLine) => {
   let lineLength;
   let pointA = line[0];
   let pointB = line[1];
   let intersection = getIntersection(line, radius * 2);
-
   let cloud = ['M ' + line[0][0] + ', ' + line[0][1]];
+
   while (intersection) {
-    cloud.push(' A ' + radius + ' ' + radius + ' 0 1 1 ' + intersection[0] + ' ' + intersection[1]);
+    cloud.push(' A ' + radius + ' ' + radius + ' 0 1 1 ' +
+      intersection[0] + ' ' + intersection[1]);
     line = [intersection, line[1]];
     intersection = getIntersection(line, radius * 2);
   }
 
-
-  // if (nextLine) {
-  //   let nextPoint = lineSegmentIntersection(
-  //     {
-  //       x: line[0][0] + radius,
-  //       y: line[0][1]
-  //     },
-  //     radius,
-  //     {
-  //       x: nextLine[0][0],
-  //       y: nextLine[0][1]
-  //     },
-  //     {
-  //       x: nextLine[1][0],
-  //       y: nextLine[1][1]
-  //     }
-  //   );
-  //
-  //   if (null !== nextPoint) {
-  //     cloud.push(' A ' + radius + ' ' + radius + ' 0 1 1 ' + nextPoint[0] + ' ' + nextPoint[1]);
-  //   }
-  // }
   return cloud.join('');
 };
 
@@ -132,6 +108,7 @@ var fixRadiusToFitLine = (line, radius) => {
   const length = lineLength(line);
   let segments = Math.round(length / (2 * radius) + 0.5);
   const newRadius = length / segments;
+
   return length / segments / 2;
 };
 
@@ -147,10 +124,12 @@ let svgCloud = (poligon, radius) => {
     cloud += cloudALine(line, fixRadiusToFitLine(line, radius));
   }
 
+  //Last line (last point to first point)
   line = [poligon[ind - 1], poligon[0]];
   cloud += cloudALine(line, fixRadiusToFitLine(line, radius));
 
   return cloud;
 };
+
 
 module.exports = svgCloud;
